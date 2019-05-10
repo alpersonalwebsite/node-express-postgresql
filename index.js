633bdbe5f04f7f1070b03cd1a6db23ba483708c1
alpersonalwebsite <mypersonalwebsite@outlook.com>
2019-05-10 13:52:32 -0700

    linting

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.get('/', (req, res, next) => {
   res.json({
