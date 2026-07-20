d5b0169765980e0c86e2fcf429b0224d3c7014a3
Al Diaz <mypersonalwebsite@outlook.com>
2026-07-20 16:29:42 -0700

    Use the cors package instead of a hand-rolled header
    cors was already a dependency but unused, while server.js set
    Access-Control-Allow-Origin manually. Replace the custom middleware with
    cors() so CORS is handled by the dedicated package.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
 
@@ -19,11 +20,7 @@ app.use(express.json())
 
 app.use(morgan('dev'))
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+app.use(cors())
 
 app.use('/api/users', userRouter)
 
